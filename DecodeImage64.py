407a240d6653cbeca64b65cdfdf75eb2ae31d611
Jeyfrin <jeyfrin.julian@gmail.com>
2016-10-06 10:56:35 -0500

    Create DecodeImage64.py

---
+++ DecodeImage64.py
@@ -0,0 +1,20 @@
+import base64
+import io
+from kivy.base import runTouchApp
+from kivy.core.image import Image as CoreImage
+from kivy.uix.image import Image
+
+"""
+pathstring64 is the last argument to read the file where this image base64 encoded and be rendered.
+"""
+
+class DecodeImage64(Image):
+    def __init__(self, pathstring64, **k):
+        super(DecodeImage64, self).__init__(**k)
+        self.source="i.jpg"
+        self._data = io.BytesIO(base64.b64decode(open(pathstring64,'r').read()))
+        self.coreimage=CoreImage(self._data, ext="jpg")
+        self.texture=self.coreimage.texture
+                
+
+runTouchApp(DecodeImage64("path/to/img64.txt"))
